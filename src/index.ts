25dee635975b0012cdc4b5d29f2d8911a184a0ea
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-02-01 09:49:00 +0200

    Read ibReportPath from properties.json

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import * as fs from "fs";
+import path from "path";
+
+const propertiesFilePath = path.join(__dirname, "..", "properties.json");
+
+const invalidPropsError = Error(
+  'You need to create properties.json file in the project root with content {"ibReportPath": "path_to_your_report"}'
+);
+
+if (!fs.existsSync(propertiesFilePath)) {
+  throw invalidPropsError;
+}
+
+const properties: { ibReportPath?: string } = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "..", "properties.json"), "utf8")
+);
+
+const ibReportPath = properties.ibReportPath;
+
+if (!ibReportPath) {
+  throw invalidPropsError;
+}
+
+console.log(ibReportPath);
