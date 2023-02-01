a727409d45b47c3cb0b1b4b9e904eaa427a65318
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-02-01 13:53:54 +0200

    Organize code

---
+++ src/csvRowToTransaction.ts
@@ -0,0 +1,35 @@
+import { DateTime } from "luxon";
+
+// TODO is it always this or the exchange time zone of traded asset?
+const tradeDataTimeZone = "America/New_York";
+
+export function csvRowToTransaction(row: string[]) {
+  // https://www.ibkrguides.com/reportingreference/reportguide/trades_default.htm
+  const [
+    currency,
+    symbol,
+    dateTime,
+    quantity,
+    tPrice, // transaction price
+    cPrice, // closing price
+    proceeds, // how much USD balance changed (excl. fee)
+    commFee,
+    basis,
+    realizedPnl,
+    mtmPnl,
+    code,
+  ] = row.slice(4);
+
+  return {
+    symbol,
+    currency,
+    dateTime: DateTime.fromFormat(dateTime, "yyyy-MM-dd, HH:mm:ss", {
+      zone: tradeDataTimeZone,
+    }),
+    quantity: parseFloat(quantity),
+    price: parseFloat(tPrice),
+    proceeds: parseFloat(proceeds),
+    fee: parseFloat(commFee),
+    realizedPnl: parseFloat(realizedPnl),
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,24 @@
 import { parse } from "csv/sync";
 import * as fs from "fs";
-import { DateTime } from "luxon";
-import { filter, pipe } from "remeda";
+import { filter, map, pipe } from "remeda";
+import { csvRowToTransaction } from "./csvRowToTransaction";
 import { getEurUsd } from "./getEurUsd";
 import { readProperties } from "./properties";
 
-// TODO is it always this or the exchange time zone of traded asset?
-const tradeDataTimeZone = "America/New_York";
-
-const data: string[][] = pipe(
+const transactions = pipe(
   readProperties().ibReportPath,
   (path) => fs.readFileSync(path, "utf8"),
   (str) =>
     parse(str, {
       relaxColumnCount: true,
       relaxQuotes: true, // Notes in the end of the CSV contain un-escaped quotes
-    })
-);
-
-const stockTradeRows = pipe(
-  data,
-  filter(
-    (row) => row[0] === "Trades" && row[1] === "Data" && row[3] === "Stocks"
-  )
-);
-
-const transactions = stockTradeRows.map((row) => {
-  // https://www.ibkrguides.com/reportingreference/reportguide/trades_default.htm
-  const [
-    currency,
-    symbol,
-    dateTime,
-    quantity,
-    tPrice, // transaction price
-    cPrice, // closing price
-    proceeds, // how much USD balance changed (excl. fee)
-    commFee,
-    basis,
-    realizedPnl,
-    mtmPnl,
-    code,
-  ] = row.slice(4);
-
-  return {
-    symbol,
-    currency,
-    dateTime: DateTime.fromFormat(dateTime, "yyyy-MM-dd, HH:mm:ss", {
-      zone: tradeDataTimeZone,
     }),
-    quantity: parseFloat(quantity),
-    price: parseFloat(tPrice),
-    proceeds: parseFloat(proceeds),
-    fee: parseFloat(commFee),
-    realizedPnl: parseFloat(realizedPnl),
-  };
-});
+  filter(
+    (row: string[]) =>
+      row[0] === "Trades" && row[1] === "Data" && row[3] === "Stocks"
+  ),
+  map(csvRowToTransaction)
+);
 
 console.log(transactions.slice(0, 2));
 
