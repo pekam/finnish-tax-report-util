b62e8429a4885912c67645fc93eebfb7b99f073c
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-03-30 13:42:04 +0300

    Add more pure way of getting EURUSD prices

--- src/getEurUsd.ts
+++ src/getEurUsd.ts
@@ -1,6 +1,7 @@
 import { parse } from "csv/sync";
 import * as fs from "fs";
 import { DateTime } from "luxon";
+import { mapToObj } from "remeda";
 import { readProperties } from "./properties";
 
 const eurUsdDateFormat = "MM/dd/yyyy";
@@ -10,6 +11,24 @@ const path = readProperties().eurUsdPath;
 const rawData: string[][] = parse(
   fs.readFileSync(path, "utf8").replaceAll('"', "") // all values are quoted
 );
+
+export type EurUsdMap = Record<string, number | undefined>;
+
+export function getEurUsdMap(): EurUsdMap {
+  return mapToObj(rawData, (row) => [row[0], parseFloat(row[1])]);
+}
+
+export function getEurUsdForDate(
+  dateTime: DateTime,
+  eurUsdMap: EurUsdMap
+): number {
+  const formattedDate = dateTime.toFormat(eurUsdDateFormat);
+  const eurUsd = eurUsdMap[formattedDate];
+  if (eurUsd === undefined) {
+    throw Error("No EUR/USD price found for date " + formattedDate);
+  }
+  return eurUsd;
+}
 
 export function getEurUsd(dateTime: DateTime): number {
   const formattedDate = dateTime.toFormat(eurUsdDateFormat);
