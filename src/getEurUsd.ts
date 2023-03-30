c99cae6c814ee94bf7d64a585f333088554f784c
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-03-30 16:24:12 +0300

    Use ISO date format in EurUsdMap

--- src/getEurUsd.ts
+++ src/getEurUsd.ts
@@ -4,8 +4,6 @@ import { DateTime } from "luxon";
 import { mapToObj } from "remeda";
 import { readProperties } from "./properties";
 
-const eurUsdDateFormat = "MM/dd/yyyy";
-
 export type EurUsdMap = Record<string, number | undefined>;
 
 export function getEurUsdMap(): EurUsdMap {
@@ -15,14 +13,21 @@ export function getEurUsdMap(): EurUsdMap {
     fs.readFileSync(path, "utf8").replaceAll('"', "") // all values are quoted
   );
 
-  return mapToObj(rawData, (row) => [row[0], parseFloat(row[1])]);
+  return mapToObj(rawData, (row) => [
+    convertDateFormatToISO(row[0]),
+    parseFloat(row[1]),
+  ]);
 }
 
 export function getEurUsd(dateTime: DateTime, eurUsdMap: EurUsdMap): number {
-  const formattedDate = dateTime.toFormat(eurUsdDateFormat);
-  const eurUsd = eurUsdMap[formattedDate];
+  const dateStr = dateTime.toISODate();
+  const eurUsd = eurUsdMap[dateStr];
   if (eurUsd === undefined) {
-    throw Error("No EUR/USD price found for date " + formattedDate);
+    throw Error("No EUR/USD price found for date " + dateStr);
   }
   return eurUsd;
+}
+
+function convertDateFormatToISO(date: string): string {
+  return DateTime.fromFormat(date, "MM/dd/yyyy").toISODate();
 }
