73e797ecccdf4020ab13ee095a6a54b5f950da86
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-04-01 19:26:35 +0300

    Add more tests for closing positions

--- test/closeEntries.test.ts
+++ test/closeEntries.test.ts
@@ -100,3 +100,79 @@ it("should reduce entry in two parts and remove entry", () => {
     }
   `);
 });
+
+it("should remove oldest entry and reduce the next entry", () => {
+  const state = process([
+    { quantity: 2, price: 5 },
+    { quantity: 10, price: 6 },
+    { quantity: -5, price: 10 },
+  ]);
+
+  expect(state).toMatchInlineSnapshot(`
+    {
+      "handled": [
+        {
+          "balanceChangeEurExcludingFees": -10,
+          "closedPnlExcludingFees": 0,
+          "eurUsd": 1,
+          "feeEur": 1,
+          "feeUsd": 1,
+          "price": 5,
+          "quantity": 2,
+          "symbol": "foo",
+          "time": "2022-01-01T00:00:00.000+02:00",
+        },
+        {
+          "balanceChangeEurExcludingFees": -60,
+          "closedPnlExcludingFees": 0,
+          "eurUsd": 1,
+          "feeEur": 1,
+          "feeUsd": 1,
+          "price": 6,
+          "quantity": 10,
+          "symbol": "foo",
+          "time": "2022-01-01T00:00:00.000+02:00",
+        },
+        {
+          "balanceChangeEurExcludingFees": 50,
+          "closedPnlExcludingFees": 22,
+          "eurUsd": 1,
+          "feeEur": 1,
+          "feeUsd": 1,
+          "price": 10,
+          "quantity": -5,
+          "symbol": "foo",
+          "time": "2022-01-01T00:00:00.000+02:00",
+        },
+      ],
+      "unclosed": {
+        "foo": [
+          {
+            "balanceChangeEurExcludingFees": -60,
+            "closedPnlExcludingFees": 0,
+            "eurUsd": 1,
+            "feeEur": 1,
+            "feeUsd": 1,
+            "price": 6,
+            "quantity": 10,
+            "remaining": 7,
+            "symbol": "foo",
+            "time": "2022-01-01T00:00:00.000+02:00",
+          },
+        ],
+      },
+    }
+  `);
+});
+
+it("should throw when single transaction closes and opens position", () => {
+  // Note: This is a valid scenario that should not throw, but handling this
+  // case is not supported at the moment. The test exists just to make sure that
+  // it throws instead of causing unexpected results.
+  expect(() =>
+    process([
+      { quantity: 2, price: 5 },
+      { quantity: -5, price: 10 },
+    ])
+  ).toThrowError();
+});
