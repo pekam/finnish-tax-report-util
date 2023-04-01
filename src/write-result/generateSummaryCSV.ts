fab17b79a0e50cc3c030ba4199faaf4adbdadf82
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-04-01 22:51:53 +0300

    Generate summary CSV file

---
+++ src/write-result/generateSummaryCSV.ts
@@ -0,0 +1,37 @@
+import { stringify } from "csv/sync";
+import { filter, pipe, sumBy } from "remeda";
+import { HandledTransaction } from "..";
+
+export function generateSummaryCSV(transactions: HandledTransaction[]) {
+  const totalSales = pipe(
+    transactions,
+    filter((t) => t.quantity < 0),
+    sumBy((t) => t.balanceChangeEurExcludingFees)
+  );
+
+  const totalWins = pipe(
+    transactions,
+    filter((t) => t.closedPnlExcludingFees > 0),
+    sumBy((t) => t.closedPnlExcludingFees)
+  );
+
+  const totalLosses = pipe(
+    transactions,
+    filter((t) => t.closedPnlExcludingFees < 0),
+    sumBy((t) => t.closedPnlExcludingFees)
+  );
+
+  const totalFees = pipe(
+    transactions,
+    sumBy((t) => t.feeEur)
+  );
+
+  const summaryCSV = stringify([
+    ["total sales (EUR)", totalSales],
+    ["total wins (EUR)", totalWins],
+    ["total losses (EUR)", totalLosses],
+    ["total fees (EUR)", totalFees],
+  ]);
+
+  return summaryCSV;
+}
--- src/write-result/writeResult.ts
+++ src/write-result/writeResult.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 import { State } from "../process-transactions/processTransactions";
 import { getProperties } from "../properties";
 import { generatePositionsCSV } from "./generatePositionsCSV";
+import { generateSummaryCSV } from "./generateSummaryCSV";
 import { generateTransactionsCSV } from "./generateTransactionsCSV";
 
 export function writeResult(state: State) {
@@ -16,4 +17,7 @@ export function writeResult(state: State) {
 
   const positionsCSV = generatePositionsCSV(state.unclosed);
   fs.writeFileSync(path.join(resultDirPath, "positions.csv"), positionsCSV);
+
+  const summaryCSV = generateSummaryCSV(state.handled);
+  fs.writeFileSync(path.join(resultDirPath, "summary.csv"), summaryCSV);
 }
